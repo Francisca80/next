1b390d925f42618c0eca7f4f51f014206f365706
francisca80 <franciscavanmaurik@gmail.com>
2024-11-11 16:48:56 +0100

    removed comments

--- margin-top/src/app/cases/page.tsx
+++ margin-top/src/app/cases/page.tsx
@@ -23,27 +23,27 @@ const Cases: React.FC = () => {
         <p className="text-xl text-left text-gray-700 mb-8">Met trots werken we aan projecten van onze klanten! We helpen graag merken te laten groeien door slimme, digitale oplossingen</p>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {caseData.map((caseItem, index) => ( // Show all cases
-            <Link href={`/cases/${caseItem.id}`} key={caseItem.id} className="block"> // Wrapped the tile in Link
+          {caseData.map((caseItem, index) => (
+            <Link href={`/cases/${caseItem.id}`} key={caseItem.id} className="block">
               <div 
-                className={`border border-gray-200 rounded-lg overflow-hidden transition-transform transform hover:scale-105 cursor-pointer ${backgroundColors[index % backgroundColors.length]}`} // Removed cursor-pointer
+                className={`border border-gray-200 rounded-lg overflow-hidden transition-transform transform hover:scale-105 cursor-pointer ${backgroundColors[index % backgroundColors.length]}`} 
               >
-                <div className="bg-white"> {/* White background for the image part */}
+                <div className="bg-white">
                   <Image 
                     src={caseItem.image} 
                     alt={`Image for ${caseItem.title}`} 
-                    width={500} // Set appropriate width
-                    height={300} // Set appropriate height
-                    className="w-full h-48 object-cover" // Changed to object-cover for better image fitting
+                    width={500} 
+                    height={300} 
+                    className="w-full h-48 object-cover" 
                     loading="lazy"
                   />
                 </div>
-                <div className="p-6 flex flex-col justify-between" style={{ height: '250px' }}> {/* Flexbox for alignment */}
+                <div className="p-6 flex flex-col justify-between" style={{ height: '250px' }}> 
                   <div>
                     <h3 className="text-xl font-semibold mb-2 text-white">{caseItem.title}</h3>
-                    <p className="text-white line-clamp-5">{caseItem.description}</p> {/* Description in gray for contrast */}
+                    <p className="text-white line-clamp-5">{caseItem.description}</p> 
                   </div>
-                  <span className="text-white text-xl hover:underline mt-4 inline-block self-end">Lees meer <FaArrowRight className="inline" /></span> {/* Moved the link text here */}
+                  <span className="text-white text-xl hover:underline mt-4 inline-block self-end">Lees meer <FaArrowRight className="inline" /></span> 
                 </div>
               </div>
             </Link>
