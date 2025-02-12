e10a87167009a6d87076c5e9428d73de9a3260b7
francisca80 <francisca@margin-top.com>
2025-02-12 13:57:56 +0100

    cleanup sanity and sendgrid

--- margin-top/src/middleware.ts
+++ margin-top/src/middleware.ts
@@ -3,9 +3,9 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
   locales: ['en', 'nl'] as const,
   defaultLocale: 'nl',
-  localePrefix: 'as-needed'
+  localePrefix: 'always'
 });
 
 export const config = {
-  matcher: ['/', '/(nl|en)/:path*']
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
